e957418c5387e5d6bebfe87cf98c8bc1cbe57856
LeonidLodygin <st108157@student.spbu.ru>
2023-10-05 16:37:11 +0300

    Add crouch movement

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,32 +4,37 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    [SerializeField] private PlayerInput PlayerInput;
+    [SerializeField] private PlayerInput playerInput;
     
     public Vector2 Movement { get; private set; }
     public Vector2 Look { get; private set; }
     public bool Run { get; private set; }
+    public bool Crouch { get; private set; }
 
     private InputActionMap currentMap;
     private InputAction moveAction;
     private InputAction lookAction;
     private InputAction runAction;
+    private InputAction crouchAction;
 
     private void Awake()
     {
         HideCursor();
-        currentMap = PlayerInput.currentActionMap;
+        currentMap = playerInput.currentActionMap;
         moveAction = currentMap.FindAction("Movement");
         lookAction = currentMap.FindAction("Look");
         runAction = currentMap.FindAction("Run");
+        crouchAction = currentMap.FindAction("Crouch");
 
         moveAction.performed += OnMove;
         lookAction.performed += OnLook;
         runAction.performed += OnRun;
-        
+        crouchAction.performed += OnCrouch;
+
         moveAction.canceled += OnMove;
         lookAction.canceled += OnLook;
         runAction.canceled += OnRun;
+        crouchAction.canceled += OnCrouch;
     }
 
     private void OnMove(InputAction.CallbackContext context)
@@ -43,6 +48,10 @@ public class InputManager : MonoBehaviour
     private void OnRun(InputAction.CallbackContext context)
     {
         Run = context.ReadValueAsButton();
+    }
+    private void OnCrouch(InputAction.CallbackContext context)
+    {
+        Crouch = context.ReadValueAsButton();
     }
 
     private void OnEnable()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,17 @@ public class PlayerController : MonoBehaviour
     private bool hasAnimator;
     private int xVelocityHash;
     private int yVelocityHash;
-    [SerializeField] private Transform CameraRoot;
-    [SerializeField] private Transform Camera;
+    private int crouchHash;
+
+    [SerializeField] private Transform сameraRoot;
+    [SerializeField] private Transform сamera;
     private Vector2 currentVelocity;
     private float xRotation;
 
+    private const float crouchSpeed = 1.5f;
     private const float walkSpeed = 2f;
     private const float runSpeed = 6f;
-    [SerializeField] private float AnimationBlendSpeed = 8.9f;
+    [SerializeField] private float animationBlendSpeed = 8.9f;
     [SerializeField] private float upperLimit = -90f;
     [SerializeField] private float bottomLimit = 90f;
     [SerializeField] private float sensitivity = 100f;
@@ -28,11 +31,13 @@ public class PlayerController : MonoBehaviour
 
         xVelocityHash = Animator.StringToHash("xVelocity");
         yVelocityHash = Animator.StringToHash("yVelocity");
+        crouchHash = Animator.StringToHash("Crouch");
     }
 
     private void FixedUpdate()
     {
         Move();
+        Crouch();
     }
 
     private void LateUpdate()
@@ -43,33 +48,35 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         if (!hasAnimator) return;
-        float targetSpeed = inputManager.Run ? runSpeed : walkSpeed;
-        if (inputManager.Movement == Vector2.zero) targetSpeed = 0.1f;
+        var targetSpeed = inputManager.Run ? runSpeed : inputManager.Crouch ? crouchSpeed : walkSpeed;
+        if (inputManager.Movement == Vector2.zero) targetSpeed = 0;
 
-        currentVelocity.x = Mathf.Lerp(currentVelocity.x, inputManager.Movement.x * targetSpeed, AnimationBlendSpeed * Time.fixedDeltaTime);
-        currentVelocity.y = Mathf.Lerp(currentVelocity.y, inputManager.Movement.y * targetSpeed, AnimationBlendSpeed * Time.fixedDeltaTime);
+        currentVelocity.x = Mathf.Lerp(currentVelocity.x, inputManager.Movement.x * targetSpeed, animationBlendSpeed * Time.fixedDeltaTime);
+        currentVelocity.y = Mathf.Lerp(currentVelocity.y, inputManager.Movement.y * targetSpeed, animationBlendSpeed * Time.fixedDeltaTime);
 
         var velocity = rigidbody.velocity;
         var xVelocityDifference = currentVelocity.x - velocity.x;
-        var yVelocityDifference = currentVelocity.y - velocity.y;
-        
+        var yVelocityDifference = currentVelocity.y - velocity.y;   
+    
         rigidbody.AddForce(transform.TransformVector(new Vector3(xVelocityDifference, 0, yVelocityDifference)), ForceMode.VelocityChange);
-        
+
         animator.SetFloat(xVelocityHash, currentVelocity.x);
         animator.SetFloat(yVelocityHash, currentVelocity.y);
     }
+
+    private void Crouch() => animator.SetBool(crouchHash, inputManager.Crouch);
 
     private void CameraMovement()
     {
         if (!hasAnimator) return;
         var mouseX = inputManager.Look.x;
         var mouseY = inputManager.Look.y;
-        Camera.position = CameraRoot.position;
+        сamera.position = сameraRoot.position;
 
-        xRotation -= mouseY * sensitivity * Time.deltaTime;
+        xRotation -= mouseY * sensitivity * Time.smoothDeltaTime;
         xRotation = Mathf.Clamp(xRotation, upperLimit, bottomLimit);
         
-        Camera.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        transform.Rotate(Vector3.up, mouseX * sensitivity * Time.deltaTime);
+        сamera.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(0, mouseX * sensitivity * Time.smoothDeltaTime, 0));
     }
 }
